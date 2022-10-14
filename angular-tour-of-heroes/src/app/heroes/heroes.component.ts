a7e1d7c707fc00ce2e0d418d00def2acce65c80f
Kate <Kate@mail.com>
2022-10-14 15:03:36 +0800

    20221014

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'; //引入 hero.ts
+import { Hero } from '../hero'; //引入 hero.ts (step2)
+import { HEROES } from '../mock-heroes'; //引用模擬的資料(step 5)
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  // step 6 : 
+  styleUrls: ['./heroes.component.scss'] //(紫白)<--這個地址，是可以自由切換的。而且只影響到自己，不影響他人。
+  //styleUrls: ['../app.component.scss'] //(黑白)<--我設定了兩者不同的 button onHover 顏色，可以看一下效果。
 })
 export class HeroesComponent implements OnInit {
   
@@ -12,15 +15,26 @@ export class HeroesComponent implements OnInit {
   // hero = 'Windstorm';
   
   // step 2
-  hero: Hero = { //才可以自由地使用它…
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = { //才可以自由地使用它…
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  // step 5
+  heroes = HEROES;
   
   constructor() { }
 
   ngOnInit(): void {
-
   }
+
+  selectedHero?: Hero; //設定有這個變數，但是不要為它賦值
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;//等有人按下按鈕時，才賦值。
+  }
+
+  
+
+
 
 }
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+// 最終，你會從遠端的資料伺服器獲取它。不過目前，你要先建立一些模擬的英雄資料，並假裝它們是從伺服器上取到的。
